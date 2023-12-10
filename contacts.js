30fba10a3d82c56b1d552c543e429660fba3c483
NPC-Roman <128088564+NPC-Roman@users.noreply.github.com>
2023-12-10 18:13:53 +0200

    change05

---
+++ contacts.js
@@ -0,0 +1,86 @@
+const fs = require("fs");
+const path = require("path");
+
+const contactsPath = path.resolve("./db/contacts.json");
+
+function listContacts() {
+  fs.readFile(contactsPath, "utf8", function (error, data) {
+    if (error) {
+      console.log(error.message);
+    }
+    const contacts = JSON.parse(data);
+    console.table(contacts);
+  });
+}
+
+function getContactById(contactId) {
+  fs.readFile(contactsPath, "utf8", function (error, data) {
+    if (error) {
+      console.log(error);
+    }
+
+    const contacts = JSON.parse(data);
+
+    contacts.find((contact) => {
+      if (Number(contact.id) === contactId) {
+        return console.log(`contact with ID ${contactId} in contact List`);
+      }
+      // console.log("Contact is not found");
+    });
+  });
+}
+
+function removeContact(contactId) {
+  fs.readFile(contactsPath, "utf8", function (error, data) {
+    if (error) {
+      console.log(error);
+    }
+
+    const contacts = JSON.parse(data);
+
+    const filterContacts = contacts.filter(
+      (contact) => Number(contact.id) !== contactId
+    );
+
+    console.table(filterContacts);
+    fs.writeFile(
+      contactsPath,
+      JSON.stringify(filterContacts),
+      function (error) {
+        if (error) {
+          return console.log(error);
+        }
+      }
+    );
+  });
+}
+
+function addContact(name, email, phone) {
+  fs.readFile(contactsPath, "utf8", function (error, data) {
+    if (error) {
+      return console.log(error);
+    }
+
+    const contacts = JSON.parse(data);
+    contacts.push({
+      name,
+      email,
+      phone,
+      id: contacts.length + 1,
+    });
+
+    console.table(contacts);
+    fs.writeFile(contactsPath, JSON.stringify(contacts), function (error) {
+      if (error) {
+        return console.log(error);
+      }
+    });
+  });
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
